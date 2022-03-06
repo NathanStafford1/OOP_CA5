1a2d1651d7b4e821d64625cf9c8070f9c092e447
Nathan <d00236171@student.dkit.ie>
2022-03-06 19:15:52 +0000

    Added Treemap and display in order

--- src/CA5/App.java
+++ src/CA5/App.java
@@ -1,60 +1,47 @@
 package CA5;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.TreeMap;
-import java.util.Map;
+import java.util.*;
 import java.util.Scanner;
 
-public class App
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) {
         App app = new App();
         app.start();
     }
-    public void start()
-    {
-        try
-        {
+
+    public void start() {
+        try {
             displayMainMenu();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         System.out.println("Program ending, Goodbye");
     }
-    private void displayMainMenu() throws IOException
-    {
+
+    private void displayMainMenu() throws IOException {
 
         final String MENU_ITEMS = "\n*** MAIN MENU OF OPTIONS ***\n"
                 + "1. Fill arraylist and display\n"
                 + "2. HashMap\n"
-                + "3. Bookings\n"
+                + "3. Treemap\n"
                 + "4. Exit\n"
                 + "Enter Option [1,4]";
 
         final int ArrayList = 1;
         final int hashMap = 2;
-        final int BOOKINGS = 3;
+        final int TREEMAP = 3;
         final int EXIT = 4;
 
         Scanner keyboard = new Scanner(System.in);
         int option = 0;
-        do
-        {
+        do {
             System.out.println("\n" + MENU_ITEMS);
-            try
-            {
+            try {
                 String usersInput = keyboard.nextLine();
                 option = Integer.parseInt(usersInput);
-                switch (option)
-                {
+                switch (option) {
                     case ArrayList:
                         System.out.println("Fill ArrayList");
                         displayArrayList();
@@ -63,9 +50,9 @@ public class App
                         System.out.println("HashMap select using key");
                         hashMap();
                         break;
-                    case BOOKINGS:
-                        System.out.println("Bookings option chosen");
-                        //displayBookingsMenu();
+                    case TREEMAP:
+                        System.out.println("Treemap order of items");
+                        TreeMap();
                         break;
                     case EXIT:
                         System.out.println("Exit Menu option chosen");
@@ -75,8 +62,7 @@ public class App
                         break;
                 }
 
-            } catch (InputMismatchException | NumberFormatException e)
-            {
+            } catch (InputMismatchException | NumberFormatException e) {
                 System.out.print("Invalid option - please enter number in range");
             }
         } while (option != EXIT);
@@ -84,52 +70,72 @@ public class App
         System.out.println("\nExiting Main Menu, goodbye.");
 
     }
-    void displayArrayList()
-    {
-        ArrayList<Game> Games = new ArrayList();
-        Games.add(new Game("Halo",49.99,5));
-        Games.add(new Game("Forza",59.99,17));
-        Games.add(new Game("Minecraft",19.99,37));
-        Games.add(new Game("Fifa",49.99,11));
-        Games.add(new Game("NBA",49.99,24));
-        Games.add(new Game("Rocket league",29.99,19));
-        Games.add(new Game("Fortnite",0.0,100));
-        Games.add(new Game("F1",49.99,14));
-        Games.add(new Game("Call of duty",49.99,5));
-        Games.add(new Game("Borderlands",39.99,17));
 
-        for (int i = 0; i < Games.size(); i++)
-        {
+    void displayArrayList() {
+        ArrayList<Game> Games = new ArrayList();
+        Games.add(new Game("Halo", 49.99, 5));
+        Games.add(new Game("Forza", 59.99, 17));
+        Games.add(new Game("Minecraft", 19.99, 37));
+        Games.add(new Game("Fifa", 49.99, 11));
+        Games.add(new Game("NBA", 49.99, 24));
+        Games.add(new Game("Rocket league", 29.99, 19));
+        Games.add(new Game("Fortnite", 0.0, 100));
+        Games.add(new Game("F1", 49.99, 14));
+        Games.add(new Game("Call of duty", 49.99, 5));
+        Games.add(new Game("Borderlands", 39.99, 17));
+
+        for (int i = 0; i < Games.size(); i++) {
             System.out.println(Games.get(i));
         }
     }
-    void hashMap()
-    {
-        Scanner scanner = new Scanner(System.in);
-        Map<Integer, Game> GameMap = new HashMap<>();
 
-        GameMap.put(1,new Game("Halo",49.99,5));
-        GameMap.put(2,new Game("Forza",59.99,17));
-        GameMap.put(3,new Game("Minecraft",19.99,37));
-        GameMap.put(4,new Game("NBA",49.99,24));
-        GameMap.put(5,new Game("Fifa",49.99,11));
-        GameMap.put(6,new Game("Rocket league",29.99,19));
-        GameMap.put(7,new Game("Fortnite",0.0,100));
-        GameMap.put(8,new Game("F1",49.99,14));
-        GameMap.put(9,new Game("Call of duty",49.99,5));
-        GameMap.put(10,new Game("Borderlands",39.99,17));
+    void hashMap() {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, Game> GameMap = new HashMap<>();
+
+        GameMap.put(1, new Game("Halo", 49.99, 5));
+        GameMap.put(2, new Game("Forza", 59.99, 17));
+        GameMap.put(3, new Game("Minecraft", 19.99, 37));
+        GameMap.put(4, new Game("NBA", 49.99, 24));
+        GameMap.put(5, new Game("Fifa", 49.99, 11));
+        GameMap.put(6, new Game("Rocket league", 29.99, 19));
+        GameMap.put(7, new Game("Fortnite", 0.0, 100));
+        GameMap.put(8, new Game("F1", 49.99, 14));
+        GameMap.put(9, new Game("Call of duty", 49.99, 5));
+        GameMap.put(10, new Game("Borderlands", 39.99, 17));
 
         System.out.println("Please enter key to search map for: ");
         int key = scanner.nextInt();
 
-        if (GameMap.containsKey(key))
-        {
+        if (GameMap.containsKey(key)) {
             System.out.println("Key found: ");
             System.out.println(GameMap.get(key));
-        }
-        else
-        {
+        } else {
             System.out.println("No key found in map");
+        }
+    }
+
+    void TreeMap() {
+        TreeMap<Long, Game> GameMap = new TreeMap<>();
+
+        GameMap.put(184723L, new Game("Halo", 49.99, 5));
+        GameMap.put(24363L, new Game("Forza", 59.99, 17));
+        GameMap.put(343865L, new Game("Minecraft", 19.99, 37));
+        GameMap.put(4352354L, new Game("NBA", 49.99, 24));
+        GameMap.put(54234L, new Game("Fifa", 49.99, 11));
+        GameMap.put(643218L, new Game("Rocket league", 29.99, 19));
+        GameMap.put(753925L, new Game("Fortnite", 0.0, 100));
+        GameMap.put(8538L, new Game("F1", 49.99, 14));
+        GameMap.put(941952L, new Game("Call of duty", 49.99, 5));
+        GameMap.put(106428L, new Game("Borderlands", 39.99, 17));
+
+        Set<Long> keySet = GameMap.keySet();
+
+        for (Long key : keySet) {
+            Game game = GameMap.get(key);
+            System.out.println("Key: " + key + ", Name: "
+                    + game.getName() + ", Price:" + game.getPrice() +
+                    ", Quantity: " + game.getQuantity());
         }
     }
 }
